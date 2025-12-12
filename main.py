40348a5b0b21cea6035ea7b0946328ba1baaf117
KashinathRajesh <kashinath.rajesh11@gmail.com>
2025-12-12 09:28:03 +0530

    Added basic packet sniffer logic

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 import scapy.all as scapy
-import yaml
+
+def packet_callback(packet):
+    print(packet.summary())
 
 def main():
-    print("IDPS Initialized...")
+    print("Available Interfaces:")
+    scapy.show_interfaces()
+    
+    # Get user input
+    choice = input("\nEnter the 'Index' (e.g. 23) of the interface to sniff: ")
+    
+    # Logic to handle Index (number) vs Name (string)
+    if choice.isdigit():
+        interface = scapy.dev_from_index(int(choice))
+    else:
+        interface = choice
+
+    print(f"\n[+] Starting capture on: {interface}...\n")
+    
+    try:
+        scapy.sniff(iface=interface, prn=packet_callback, store=False)
+    except KeyboardInterrupt:
+        print("\n[!] Stopping capture.")
+    except Exception as e:
+        print(f"\n[!] Error: {e}")
 
 if __name__ == "__main__":
     main()
